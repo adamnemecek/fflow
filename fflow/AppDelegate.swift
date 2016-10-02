cb85e858440381eff888621e50e33e1b2ee8cf8c
to.git <to.git.g@gmail.com>
2016-10-02 09:50:06 +0900

    Add global event handler for scrollWheel

--- fflow/AppDelegate.swift
+++ fflow/AppDelegate.swift
@@ -13,15 +13,37 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     let statusItem = NSStatusBar.system().statusItem(withLength: NSSquareStatusItemLength)
     let setting = Setting()
+    let gestureManager = GestureManager()
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         
+        // Set status menu
         let menu = NSMenu()
         menu.addItem(makeMenuItem(title: "Quit", selector: #selector(quit)))
         statusItem.menu = menu
         statusItem.highlightMode = true
         statusItem.title = "ff"
+        
+        // Regist event handler for scrollWheel
+        NSEvent.addGlobalMonitorForEvents(
+            matching: NSEventMask.scrollWheel,
+            handler: {(evt: NSEvent!) -> Void in
+                
+                let x = evt.scrollingDeltaX
+                let y = evt.scrollingDeltaY
+                let direction: String? = Direction.which(x, y: y)?.rawValue
+                
+                guard let gesture = self.gestureManager.add(direction: direction) else { return }
+                guard let frontmostAppName = NSWorkspace.shared().frontmostApplication?.localizedName else { return }
+                guard let keyStrokes = self.setting.keyStrokes[frontmostAppName] else { return }
+                
+                guard let keyStroke = keyStrokes[gesture] else { return }
+                
+                print(keyStroke)
+//                var error: NSDictionary?
+//                keyStroke.executeAndReturnError(&error)
+        })
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
