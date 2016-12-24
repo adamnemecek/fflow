ddd0fd80c7846140695330527908f150b5e3a6bb
to.git <to.git.g@gmail.com>
2016-12-24 14:46:11 +0900

    Add NSRect and its tests

---
+++ fflow/NSRect.extension.swift
@@ -0,0 +1,69 @@
+//
+//  NSRect.extension.swift
+//  fflow
+//
+//  Created by user on 2016/12/20.
+//  Copyright © 2016年 user. All rights reserved.
+//
+
+import Cocoa
+
+extension NSRect {
+
+    var shortSide: CGFloat {
+
+        return self.size.shortSide
+    }
+
+    var longSide: CGFloat {
+
+        return self.size.longSide
+    }
+}
+
+extension NSRect {
+    
+    init(size: NSSize) {
+
+       self.init(origin: .zero, size: size)
+    }
+
+    init(width: CGFloat, height: CGFloat) {
+
+        self.init(x: 0, y: 0, width: width, height: height)
+    }
+}
+
+extension NSRect {
+
+    mutating func center(of superrect: NSRect) {
+
+        let x = (superrect.width - self.width) / 2
+        let y = (superrect.height - self.height) / 2
+
+        self.origin = .init(x: x, y: y)
+    }
+}
+
+extension NSRect {
+
+    init(center: NSPoint, size: NSSize) {
+
+        let x = center.x - size.width / 2
+        let y = center.y - size.height / 2
+        let origin = NSPoint(x: x, y: y)
+
+        self.init(origin: origin, size: size)
+    }
+}
+
+extension NSRect {
+
+    var centerPoint: NSPoint {
+
+        let x = self.minX + self.width / 2
+        let y = self.minY + self.height / 2
+
+        return NSPoint(x: x, y: y)
+    }
+}
---
+++ fflowTests/NSRectExtensionTests.swift
@@ -0,0 +1,73 @@
+//
+//  NSRect.extension.swift
+//  fflow
+//
+//  Created by user on 2016/12/21.
+//  Copyright © 2016年 user. All rights reserved.
+//
+
+import XCTest
+@testable import fflow
+
+class NSRectExtensionTests: XCTestCase {
+
+    override func setUp() {
+
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testInitWithSize() {
+
+        let rect = NSRect(size: .init(width: 20, height: 30))
+
+        XCTAssertEqual(rect.origin.x, 0)
+        XCTAssertEqual(rect.origin.y, 0)
+        XCTAssertEqual(rect.size.width, 20)
+        XCTAssertEqual(rect.size.height, 30)
+    }
+
+    func testInitWithWidthAndHeight() {
+
+        let rect = NSRect(width: 22, height: 33)
+
+        XCTAssertEqual(rect.origin.x, 0)
+        XCTAssertEqual(rect.origin.y, 0)
+        XCTAssertEqual(rect.size.width, 22)
+        XCTAssertEqual(rect.size.height, 33)
+    }
+
+    func testShortSideAndLongSide() {
+
+        let rect = NSRect(width: 22, height: 33)
+
+        XCTAssertEqual(rect.shortSide, 22)
+        XCTAssertEqual(rect.longSide, 33)
+    }
+
+    func testInitWithCenter() {
+
+        let center = NSPoint(x: 30, y: 40)
+        let size = NSSize(squaringOf: 50)
+        let rect = NSRect(center: center, size: size)
+
+        XCTAssertEqual(rect.origin.x, 5)
+        XCTAssertEqual(rect.origin.y, 15)
+        XCTAssertEqual(rect.size.width, 50)
+        XCTAssertEqual(rect.size.height, 50)
+    }
+
+    func testCenterPoint() {
+
+        let centerPoint = NSRect(x: 3, y: 4, width: 5, height: 6).centerPoint
+
+        XCTAssertEqual(centerPoint.x, 5.5)
+        XCTAssertEqual(centerPoint.y, 7)
+    }
+}
