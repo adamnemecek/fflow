9cddee9240b87ac824bbf430e68a0f5519526301
to.git <to.git.g@gmail.com>
2016-12-24 15:22:05 +0900

    Move todos to external text file

--- fflowTests/fflowTests.swift
+++ fflowTests/fflowTests.swift
@@ -11,17 +11,17 @@ import XCTest
 
 class fflowTests: XCTestCase {
     
-    override func setUp() {
-        
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+//    override func setUp() {
+//        
+//        super.setUp()
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
     
-    override func tearDown() {
-
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
+//    override func tearDown() {
+//
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//        super.tearDown()
+//    }
     
 //    func testAny() {
 //        
@@ -34,42 +34,5 @@ class fflowTests: XCTestCase {
 //            // Put the code you want to measure the time of here.
 //        }
 //    }
-
-
-
-
-
-
-
-    // TODO: appendAndReleaseIfCan() method? instead of append() and release()
-
-    // TODO: Be more cleanly Indicator class
-
-    // TODO: CommandPreference setForGlobal() method
-
-    // TODO: Preference class: has commandPreference and windowViewController
-
-    // TODO: Use synchronize()
-
-    // TODO: control option shif command
-
-
-
-
-    // TODO: Try error throwing
-
-    // TODO: Split tests by each class
-    // TODO: Split tests by each subject
-    // TODO: Enhance tests
-
-    // TODO: Do commit and update recommended setting
-
     
-    
-    // TODO: Add UnitVector class
-
-    // TODO: Place direction string validation to one block
-
-    // TODO: keystroke allows empty string ?
-
 }
