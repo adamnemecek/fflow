127a09f6fd108972ebe62b2af201b1c9bbd00699
to.git <to.git.g@gmail.com>
2016-12-24 14:41:24 +0900

    Add CGVector extension and its tests

---
+++ fflow/CGVector.extension.swift
@@ -0,0 +1,44 @@
+//
+//  CGVector.extension.swift
+//  fflow
+//
+//  Created by user on 2016/12/21.
+//  Copyright © 2016年 user. All rights reserved.
+//
+
+import Foundation
+
+extension CGVector {
+
+    static func +(lhs: CGVector, rhs: CGVector) -> CGVector {
+
+        return CGVector(dx: lhs.dx + rhs.dx,
+                        dy: lhs.dy + rhs.dy)
+    }
+    
+    static func -(lhs: CGVector, rhs: CGVector) -> CGVector {
+
+        return CGVector(dx: lhs.dx - rhs.dx,
+                        dy: lhs.dy - rhs.dy)
+    }
+
+    static func *(lhs: CGFloat, rhs: CGVector) -> CGVector {
+
+        return CGVector(dx: lhs * rhs.dx,
+                        dy: lhs * rhs.dy)
+    }
+}
+
+
+extension CGVector {
+
+    init(endPoint: NSPoint) {
+
+        self.init(dx: endPoint.x, dy: endPoint.y)
+    }
+
+    var endPoint: NSPoint {
+
+        return NSPoint(x: self.dx, y: self.dy)
+    }
+}
---
+++ fflowTests/CGVectorExtensionTests.swift
@@ -0,0 +1,59 @@
+//
+//  CGVector.swift
+//  fflow
+//
+//  Created by user on 2016/12/21.
+//  Copyright © 2016年 user. All rights reserved.
+//
+
+import XCTest
+@testable import fflow
+
+class CGVectorExtensionTests: XCTestCase {
+
+    func testPlusOperator() {
+
+        let twoThree = CGVector(dx: 2, dy: 3)
+        let fourFive = CGVector(dx: 4, dy: 5)
+
+        let sixEight = twoThree + fourFive
+        XCTAssertEqual(sixEight.dx, 6)
+        XCTAssertEqual(sixEight.dy, 8)
+    }
+
+    func testMinusOperator() {
+
+        let fourFive = CGVector(dx: 4, dy: 5)
+        let threeTwo = CGVector(dx: 3, dy: 2)
+
+        let oneThree = fourFive - threeTwo
+        XCTAssertEqual(oneThree.dx, 1)
+        XCTAssertEqual(oneThree.dy, 3)
+    }
+
+    func testMultiOperator() {
+
+        let fourFive = CGVector(dx: 4, dy: 5)
+
+        let twelveFifteen = 3 * fourFive
+        XCTAssertEqual(twelveFifteen.dx, 12)
+        XCTAssertEqual(twelveFifteen.dy, 15)
+    }
+    
+    func testInitByNSPoint() {
+
+        let point = NSPoint(x: 2, y: 3)
+        let vector = CGVector(endPoint: point)
+        
+        XCTAssertEqual(vector.dx, 2)
+        XCTAssertEqual(vector.dy, 3)
+    }
+    
+    func testEndPoint() {
+
+        let fourFive = CGVector(dx: 4, dy: 5).endPoint
+
+        XCTAssertEqual(fourFive.x, 4)
+        XCTAssertEqual(fourFive.y, 5)
+    }
+}
