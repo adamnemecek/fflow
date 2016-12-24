f5fcfe6b2cfd043e2f12abcefcf319eddbd9098f
to.git <to.git.g@gmail.com>
2016-12-24 14:57:53 +0900

    Remove view.center() method

--- fflow/NSView.extension.swift
+++ fflow/NSView.extension.swift
@@ -15,25 +15,3 @@ extension NSView {
         self.init(frame: .init(origin: .zero, size: size))
     }
 }
-
-extension NSView {
-
-    func center() {
-
-        guard let origin = self.originOfCenteredRect() else { return }
-        self.setFrameOrigin(origin)
-    }
-
-    private func originOfCenteredRect() -> NSPoint? {
-
-        guard let superview = self.superview else { return nil }
-        
-        let parent = superview.frame.size
-        let child = self.frame.size
-
-        let x = (parent.width - child.width) / 2
-        let y = (parent.height - child.height) / 2
-        
-        return NSPoint(x: x, y: y)
-    }
-}
