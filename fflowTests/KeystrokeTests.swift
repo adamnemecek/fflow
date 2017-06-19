37f69a5ef786aed736369e9a5077f19f0dff401a
to.git <to.git.g@gmail.com>
2017-06-19 18:08:23 +0900

    CLEAN: remove unnecessary setUp() and tearDown() methods from test.

--- fflowTests/KeystrokeTests.swift
+++ fflowTests/KeystrokeTests.swift
@@ -16,18 +16,6 @@ class KeystrokeTests: XCTestCase {
     let option = Key.Option.symbol
     let command = Key.Command.symbol
 
-    override func setUp() {
-
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-
     func testInit() {
 
         XCTAssertNil(Keystroke(fromString: ""))
