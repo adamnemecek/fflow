39005335fc25eaddb8b14189b5b7ea5d4e58da0d
to.git <to.git.g@gmail.com>
2017-07-14 13:05:21 +0900

    ADD: StatusItemMenu enum.

---
+++ fflow/StatusItemMenu.swift
@@ -0,0 +1,15 @@
+//
+//  StatusItemMenu.swift
+//  fflow
+//
+//  Created by user on 2017/07/14.
+//  Copyright © 2017年 user. All rights reserved.
+//
+
+import Foundation
+
+enum StatusItemMenu {
+
+    case Preferences
+    case Quit
+}
