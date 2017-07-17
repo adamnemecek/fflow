a2f911bfa6d0255d42d8e6b37bcfa8c924dbc034
to.git <to.git.g@gmail.com>
2017-07-17 11:11:39 +0900

    CHANGE: icon of status item.

--- fflow/StatusItem.swift
+++ fflow/StatusItem.swift
@@ -10,23 +10,47 @@ import Cocoa
 
 class StatusItem {
 
+    static private var iconImageName: String { return "StatusItemIcon" }
+    static private var iconImage: NSImage? { return NSImage(named: self.iconImageName) }
+
+    static private var size: NSSize { return NSSize(squaringOf: 18) }
+
+    static private var icon: NSImage {
+
+        guard let image = self.iconImage else { return NSImage.init() }
+        image.isTemplate = true
+        image.size = self.size
+        return image
+    }
+
     static private var menuItems: [NSMenuItem] {
 
         return [StatusItemMenu.Preferences.item,
                 StatusItemMenu.Quit.item]
     }
 
+    static private var menu: NSMenu {
+
+        let menu = NSMenu()
+        self.menuItems.forEach({ menu.addItem($0) })
+        return menu
+    }
+
+    static private var templateStatusItem: NSStatusItem {
+
+        let statusItem = NSStatusBar.system().statusItem(withLength: NSSquareStatusItemLength)
+        statusItem.menu = self.menu
+        statusItem.highlightMode = true
+        statusItem.image = self.icon
+        return statusItem
+    }
+
     static let shared = StatusItem()
 
-    private let statusItem = NSStatusBar.system().statusItem(withLength: NSSquareStatusItemLength)
+    private let statusItem: NSStatusItem
 
     private init() {
 
-        let menu = NSMenu()
-        StatusItem.menuItems.forEach({ menu.addItem($0) })
-
-        self.statusItem.menu = menu
-        self.statusItem.highlightMode = true
-        self.statusItem.image = StatusItemIcon.icon()
+        self.statusItem = StatusItem.templateStatusItem
     }
 }
